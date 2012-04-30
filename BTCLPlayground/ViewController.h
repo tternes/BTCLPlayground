e19db6ba3e8065a40bc6f8877070dad80efafae5
Thaddeus Ternes <thaddeus@bluetoo.co>
2012-04-29 23:25:57 -0500

    Initial Commit

---
+++ BTCLPlayground/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BTCLPlayground
+//
+//  Created by Thaddeus Ternes on 4/29/12.
+//  Copyright (c) 2012 Bluetoo Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BTCLPlayground/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BTCLPlayground
+//
+//  Created by Thaddeus Ternes on 4/29/12.
+//  Copyright (c) 2012 Bluetoo Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
